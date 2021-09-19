5b3433744125f84d6a677281c9d0130c60406eb9
JuniorBaymax <cooladitya953@gmail.com>
2021-09-19 23:00:27 +0530

    commit #2: Add SignIn with google button

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import { Container, Join, Nav, SignIn } from './login-styled';
+import {
+  Container,
+  Form,
+  Google,
+  Hero,
+  Join,
+  Nav,
+  Section,
+  SignIn,
+} from './login-styled';
 
 const Login = () => {
   return (
@@ -16,6 +25,24 @@ const Login = () => {
           <SignIn>Sign In</SignIn>
         </div>
       </Nav>
+      <Section>
+        <Hero>
+          <h1>welcome to your professional community</h1>
+          <img
+            src="https://static-exp1.licdn.com/sc/h/dxf91zhqd2z6b0bwg85ktm5s4"
+            alt="hero-img"
+          />
+        </Hero>
+        <Form>
+          <Google>
+            <img
+              src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png"
+              alt="Google"
+            />
+            Sign in with Google
+          </Google>
+        </Form>
+      </Section>
     </Container>
   );
 };
--- src/components/Login/login-styled.js
+++ src/components/Login/login-styled.js
@@ -54,5 +54,98 @@ export const SignIn = styled.a`
     background-color: rgba(121, 181, 249, 0.15);
     color: #0a66c2;
     text-decoration: none;
+    border: 1px solid #0a66c2;
+  }
+`;
+
+export const Section = styled.section`
+  display: flex;
+  align-content: flex-start;
+  min-height: 700px;
+  padding-bottom: 138px;
+  padding-top: 40px;
+  padding: 60px 0;
+  position: relative;
+  flex-wrap: wrap;
+  width: 100%;
+  max-width: 1128px;
+  align-items: center;
+  margin: auto;
+
+  @media (max-width: 768px) {
+    margin: auto;
+    min-height: 0px;
+  } ;
+`;
+
+export const Hero = styled.div`
+  width: 100%;
+
+  h1 {
+    padding-bottom: 0;
+    width: 55%;
+    font-size: 56px;
+    color: #2977c9;
+    font-weight: 200;
+    line-height: 70px;
+
+    @media (max-width: 768px) {
+      text-align: center;
+      font-size: 20px;
+      width: 100%;
+      line-height: 2;
+    }
+  }
+
+  img {
+    z-index: -1;
+    width: 700px;
+    height: 670px;
+    position: absolute;
+    bottom: -2px;
+    right: -150px;
+
+    @media (max-width: 768px) {
+      top: 230px;
+      width: initial;
+      position: initial;
+      height: initial;
+    }
+  }
+`;
+
+export const Form = styled.div`
+  margin-top: 100px;
+  width: 408px;
+
+  @media (max-width: 768px) {
+    margin-top: 20px;
+  }
+`;
+
+export const Google = styled.button`
+  display: flex;
+  justify-content: center;
+  background-color: #fff;
+  align-items: center;
+  height: 56px;
+  width: 100%;
+  border-radius: 28px;
+  box-shadow: inset 0 0 0 1px rgb(0 0 0 / 60%),
+    inset 0 0 0 2px rgb(0 0 0 / 0%) inset 0 0 0 1px rgb(0 0 0 / 0%);
+  vertical-align: middle;
+  z-index: 0;
+  transition-duration: 200ms;
+  font-size: 20px;
+  color: rgba(0, 0, 0, 0.6);
+
+  &:hover {
+    background-color: rgba(207, 207, 207, 0.25);
+    color: rgba(0, 0, 0, 0.75);
+  }
+
+  img {
+    width: 35px;
+    height: 35px;
   }
 `;
