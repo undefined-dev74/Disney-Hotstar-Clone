027372636448dd216d48ade1c6e0ed0e0919f352
JuniorBaymax <cooladitya953@gmail.com>
2021-09-19 21:33:03 +0530

    Change App to the LinkdIn clone

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return <div className="App">Aman</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/"></Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
