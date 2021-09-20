1ecae5b05a45a76fd0e0505388a097c92d8c32cc
JuniorBaymax <cooladitya953@gmail.com>
2021-09-21 00:59:50 +0530

    Commit #4 :: Added User and Work Icon, finalize the styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,19 @@
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import { Container, Content, Logo, Search, SearchIcon } from './Header.styled';
+import {
+  Container,
+  Content,
+  Logo,
+  Nav,
+  NavList,
+  NavListWrap,
+  Search,
+  SearchIcon,
+  SignOut,
+  User,
+  Work,
+} from './Header.styled';
 
 const Header = () => {
   return (
@@ -24,6 +38,82 @@ const Header = () => {
             />
           </SearchIcon>
         </Search>
+        <Nav>
+          <NavListWrap>
+            <NavList className="active">
+              <a>
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/3884/3884324.png"
+                  alt="HomeIcon"
+                />
+                <span>Home</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a>
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/681/681443.png"
+                  alt="MyNetwork"
+                />
+                <span>My Network</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a>
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/1063/1063376.png"
+                  alt="Jobs"
+                />
+                <span>Jobs</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a>
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/1370/1370907.png"
+                  alt="Messaging"
+                />
+                <span>Messaging</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a>
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/1827/1827504.png"
+                  alt="Notifications"
+                />
+                <span>Notifications</span>
+              </a>
+            </NavList>
+
+            <User>
+              <a>
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/1077/1077063.png"
+                  alt="User"
+                />
+                <span>
+                  Me
+                  <FontAwesomeIcon icon={faCaretDown} size="lg" />
+                </span>
+              </a>
+
+              <SignOut></SignOut>
+            </User>
+            <Work>
+              <a>
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/847/847455.png"
+                  alt="Work"
+                />
+                <span>
+                  Work
+                  <FontAwesomeIcon icon={faCaretDown} size="lg" />
+                </span>
+              </a>
+            </Work>
+          </NavListWrap>
+        </Nav>
       </Content>
     </Container>
   );
--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -72,4 +72,98 @@ export const SearchIcon = styled.div`
   }
 `;
 
-export const Nav = styled.nav``;
+export const Nav = styled.nav`
+  margin-left: auto;
+  display: block;
+
+  @media (max-width: 768px) {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    background-color: white;
+    width: 100%;
+  }
+`;
+
+export const NavListWrap = styled.ul`
+  display: flex;
+  flex-wrap: nowrap;
+  list-style-type: none;
+
+  .active {
+    span:after {
+      content: '';
+      transform: scaleX(1);
+      border-bottom: 2px solid #000000;
+      bottom: 0;
+      left: 0;
+      position: absolute;
+      transition: transform 0.2s ease-in-out;
+      width: 100%;
+      border-color: rgba(0, 0, 0, 0.9);
+    }
+  }
+`;
+
+export const NavList = styled.li`
+  display: flex;
+  align-items: center;
+
+  a,
+  .navicon {
+    align-items: center;
+    background: transparent;
+    display: flex;
+    cursor: pointer;
+    flex-direction: column;
+    font-size: 12px;
+    font-weight: 400;
+    justify-content: center;
+    line-height: 1.5;
+    height: 52px;
+    width: 80px;
+    position: relative;
+    text-decoration: none;
+
+    img {
+      height: 24px;
+      width: 24px;
+      min-height: auto;
+      min-width: auto;
+    }
+
+    span {
+      color: rgba(0, 0, 0, 0.6);
+      display: flex;
+      text-align: center;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      font-size: 12px;
+      line-height: 18px;
+      font-weight: normal;
+    }
+
+    @media (max-width: 768px) {
+      min-width: 70px;
+    }
+  }
+
+  &:hover,
+  &:active {
+    animation: all ease-in 2s;
+    a {
+      span {
+        color: rgba(0, 0, 0, 0.9);
+      }
+    }
+  }
+`;
+
+export const User = styled(NavList)``;
+export const Work = styled(User)`
+  border-left: 1px solid rgba(0, 0, 0, 0.8);
+`;
+
+export const SignOut = styled.div``;
